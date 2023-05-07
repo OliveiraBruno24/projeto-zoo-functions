e01406ba164e647c595304d54f25e5abf6252c61
Bruno de Oliveira <brunodeoliveira2000@hotmail.com>
2023-05-07 16:15:58 -0300

    requisito 2 - Ok aprendi sobre "residents" hj

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui 
+  const species = data.species.find((especies) => especies.name === animal);
+  if (!species) {
+    return false; // espécie não encontrada
+  }
+  return species.residents.every((bicho) => bicho.age >= age);
+  // residents é um array de objetos que tem as infos dos animais da espécie passada.
 };
-
+// console.log(getAnimalsOlderThan('lions', 1));
 module.exports = getAnimalsOlderThan;
