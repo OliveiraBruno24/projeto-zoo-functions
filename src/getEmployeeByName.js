39c6a19f0b291ad70e14658bb86a906836118003
Bruno de Oliveira <brunodeoliveira2000@hotmail.com>
2023-05-09 19:34:05 -0300

    requisito 3 Ok

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-const getEmployeeByName = (employeeName) => {
-  // seu código aqui
-};
+// const getEmployeeByName = data.employees.map((employeeName) => {
+//   const colaboradores = {};
+//   colaboradores.firstName = employeeName;
+//   colaboradores.lastName = employeeName;
+//   return colaboradores;
+// });
 
+const getEmployeeByName = (employeeName) => {
+  const funcionario = data.employees.find((employee) => {
+    const infos = employee.firstName === employeeName || employee.lastName === employeeName;
+    return infos;
+  });
+  return funcionario || {};
+};
 module.exports = getEmployeeByName;
