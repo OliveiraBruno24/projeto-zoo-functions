67cfb466230b4085f1db5d393f5d73524880c6b3
Bruno de Oliveira <brunodeoliveira2000@hotmail.com>
2023-05-11 00:46:12 -0300

    FINALMENTE ACABEI ESSE REQUISITO, só 18h pra fazer

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (animal === undefined) {
+    return data.species.reduce((acc, especie) => {
+      acc[especie.name] = especie.residents.length;
+      return acc;
+    }, {});
+  } if (animal.sex) {
+    console.log('aaa');
+    const sexoEspecifico = data.species.find((especie) => especie.name === animal.species);
+    return sexoEspecifico.residents.filter((bicho) => bicho.sex === animal.sex).length;
+  } if (typeof (animal) === 'object') {
+    const findAnimal = data.species.find((especie) => especie.name === animal.species).residents;
+    return findAnimal.length;
+  }
 };
-
+console.log(countAnimals({ species: 'bears', sex: 'male' }));
 module.exports = countAnimals;
+
+// const contaEspecie = filtraEspecie.find((especie) => especie.residents.length);
