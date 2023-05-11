5215ae23f0b95cdc0801dae3dbb29f3ec29db59b
Bruno de Oliveira <brunodeoliveira2000@hotmail.com>
2023-05-11 10:28:40 -0300

    refazendo em outro dia, tranquilamente, agr ficou bom.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,21 +1,35 @@
 const data = require('../data/zoo_data');
 
+// eslint-disable-next-line max-lines-per-function
 const countAnimals = (animal) => {
   if (animal === undefined) {
     return data.species.reduce((acc, especie) => {
       acc[especie.name] = especie.residents.length;
       return acc;
     }, {});
-  } if (animal.sex) {
-    console.log('aaa');
-    const sexoEspecifico = data.species.find((especie) => especie.name === animal.species);
-    return sexoEspecifico.residents.filter((bicho) => bicho.sex === animal.sex).length;
-  } if (typeof (animal) === 'object') {
-    const findAnimal = data.species.find((especie) => especie.name === animal.species).residents;
-    return findAnimal.length;
   }
+  // segunda versão, refatorada pós monitoria.
+  return data.species.find((especie) => especie.name === animal.species)
+    .residents.filter((bicho) => (animal.sex ? bicho.sex === animal.sex : true)).length;
+
+  // comentando solução refatorada:
+
+  // (animal.sex ? bicho.sex === animal.sex : true) --> checa se a o animal tem a consição sex definida como uma string
+  // se tiver, verifica se bicho.sex bate com o sexo especificado, se bater ok, se não só segue o baile... Next.
+  // o true ali era na intensão de "segue o baile" msm, mas testei e vi q se colocar 'pao' funciona igual... então só deixei.
+  // isso me mostrou que a sensação de conhecimento é apenas um delirio coletivo da sociedade (|-_-|).
+
+  // --- primeira versão, modo paulada ---
+
+  // } if (animal.sex) {
+  //   console.log('aaa');
+  //   const sexoEspecifico = data.species.find((especie) => especie.name === animal.species);
+  //   return sexoEspecifico.residents.filter((bicho) => bicho.sex === animal.sex).length;
+  // } if (typeof (animal) === 'object') {
+  //   const findAnimal = data.species.find((especie) => especie.name === animal.species).residents;
+  //   return findAnimal.length;
+  // }
 };
-console.log(countAnimals({ species: 'bears', sex: 'male' }));
 module.exports = countAnimals;
 
 // const contaEspecie = filtraEspecie.find((especie) => especie.residents.length);
