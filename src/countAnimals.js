32aa6350c23af9d8fd13358ef1543359fa8f3a46
Bruno de Oliveira <brunodeoliveira2000@hotmail.com>
2023-05-11 21:29:55 -0300

    requisito 4 feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -18,18 +18,17 @@ const countAnimals = (animal) => {
   // se tiver, verifica se bicho.sex bate com o sexo especificado, se bater ok, se não só segue o baile... Next.
   // o true ali era na intensão de "segue o baile" msm, mas testei e vi q se colocar 'pao' funciona igual... então só deixei.
   // isso me mostrou que a sensação de conhecimento é apenas um delirio coletivo da sociedade (|-_-|).
-
-  // --- primeira versão, modo paulada ---
-
-  // } if (animal.sex) {
-  //   console.log('aaa');
-  //   const sexoEspecifico = data.species.find((especie) => especie.name === animal.species);
-  //   return sexoEspecifico.residents.filter((bicho) => bicho.sex === animal.sex).length;
-  // } if (typeof (animal) === 'object') {
-  //   const findAnimal = data.species.find((especie) => especie.name === animal.species).residents;
-  //   return findAnimal.length;
-  // }
 };
 module.exports = countAnimals;
+// --- primeira versão, modo paulada ---
+
+// } if (animal.sex) {
+//   console.log('aaa');
+//   const sexoEspecifico = data.species.find((especie) => especie.name === animal.species);
+//   return sexoEspecifico.residents.filter((bicho) => bicho.sex === animal.sex).length;
+// } if (typeof (animal) === 'object') {
+//   const findAnimal = data.species.find((especie) => especie.name === animal.species).residents;
+//   return findAnimal.length;
+// }
 
 // const contaEspecie = filtraEspecie.find((especie) => especie.residents.length);
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,14 @@
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const isManager = (id) => data.employees.some((pessoa) => pessoa.managers.includes(id));
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId)) { // verificando se o parametro managerId existe em isManager, ou seja, é um gerente.
+    return data.employees.filter((pessoas) => pessoas.managers.includes(managerId))
+      .map((pessoas) => `${pessoas.firstName} ${pessoas.lastName}`);
+  }
+
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
 
 module.exports = { isManager, getRelatedEmployees };
